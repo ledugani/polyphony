b8c1722456d0d2c26df4aad85edde68a4e51480f
Tom Dugan <thomas@aadwebb.com>
2019-02-14 12:51:47 -0600

    removed additional homepage info

--- ClientApp/src/components/Home/Home.js
+++ ClientApp/src/components/Home/Home.js
@@ -17,7 +17,7 @@ export class Home extends Component {
                     <p><a className="btn btn-primary btn-lg" href="/registration" role="button">Sign up</a></p>
                     <p className="smaller-text">or <a href="/login">login</a></p>
                 </div>
-                <hr/>
+                {/* <hr/>
                 <div className="container">
                     <div className="row">
                         <div className="col-sm">
@@ -30,7 +30,7 @@ export class Home extends Component {
                         Embed your favorite music from Spotify, Soundcloud, or Bandcamp.
                         </div>
                     </div>
-                </div>
+                </div> */}
                 <div className="footer">
                     <p>Terms of Use &nbsp; Privacy Policy &nbsp; &copy; <a href="https://personal-site-128be.firebaseapp.com/">Tom Dugan Enterprises</a></p>
                 </div>
